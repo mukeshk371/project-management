13329db60b4957b9986f340984f79510b1f6c050
mukesh.kumar <mukesh.kumar@decimal.co.in>
2020-04-25 10:14:10 +0530

    card addition by input functionality added

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,28 @@ function addRow() {
     var cardName = document.getElementById('card-name').value;
     var cardHeader = document.createElement('div');
     cardHeader.classList.add('card-header');
+    cardHeader.classList.add('mb-1')
     cardHeader.innerHTML = cardName;
+
+    var addCardBtn = document.createElement('input');
+    addCardBtn.setAttribute('placeholder', 'Add a Card');
+    addCardBtn.setAttribute('id', 'task-name')
+
+    var addBtn = document.createElement('button');
+    addBtn.innerHTML = 'Add';
+    addBtn.setAttribute('onclick', 'addCard();');
+
+    var cardBody = document.createElement('div');
+    cardBody.classList.add('card-body');
+
     var card = document.createElement('div');
     card.classList.add('card');
     card.classList.add('p-1');
+    card.setAttribute('id', 'card');
     card.appendChild(cardHeader);
+    card.appendChild(cardBody);
+    card.appendChild(addCardBtn);
+    card.appendChild(addBtn);
 
     var cardColumn = document.createElement('div');
     cardColumn.classList.add('width-25');
@@ -15,4 +32,16 @@ function addRow() {
 
     var cardBox = document.getElementById('card-box');
     cardBox.appendChild(cardColumn);
+}
+
+function addCard() {
+    var cardName = document.getElementById('task-name').value;
+    var cardList = document.createElement('div');
+    cardList.classList.add('card');
+    cardList.classList.add('p-1');
+    cardList.classList.add('mb-1');
+    cardList.innerHTML = cardName;
+    var card = document.getElementById('card');
+    console.log(card.children);
+    card.childNodes[1].appendChild(cardList);
 }
