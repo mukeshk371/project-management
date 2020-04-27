688d6c063d64a1f790e7bace0b32538cf4a50392
mukesh.kumar <mukesh.kumar@decimal.co.in>
2020-04-27 08:45:08 +0530

    error text added for blank input

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,12 +9,15 @@ function addRow() {
     addCardBtn.setAttribute('placeholder', 'Add a Card');
     addCardBtn.classList.add('task-card-input');
     addCardBtn.classList.add('mr-05');
-    addCardBtn.setAttribute('id', 'task-name')
+    addCardBtn.setAttribute('id', 'task-name');
+
+    var errorText = document.createElement('span');
+    errorText.classList.add('error-text');
 
     var addBtn = document.createElement('button');
     addBtn.classList.add('btn-primary');
     addBtn.innerHTML = 'Add';
-    addBtn.addEventListener('click', function() {
+    addBtn.addEventListener('click', function () {
         var taskName = addCardBtn.value;
         var cardList = document.createElement('div');
         cardList.classList.add('card');
@@ -22,8 +25,10 @@ function addRow() {
         cardList.classList.add('mb-1');
         cardList.innerHTML = taskName;
         if (taskName == '') {
-            alert('Enter task name first');
+            errorText.innerHTML = 'Enter task name first';
+            errorText.style.display = 'block';
         } else {
+            errorText.style.display = 'none';
             cardBody.appendChild(cardList);
         }
     });
@@ -39,6 +44,7 @@ function addRow() {
     card.appendChild(cardBody);
     card.appendChild(addCardBtn);
     card.appendChild(addBtn);
+    card.appendChild(errorText);
 
     var cardColumn = document.createElement('div');
     cardColumn.classList.add('task-card');
@@ -46,10 +52,16 @@ function addRow() {
     cardColumn.appendChild(card);
 
     var cardBox = document.getElementById('card-box');
+    var errorText1 = document.getElementById('error-text1');
+    var bodyDiv = document.getElementById('body-div');
 
     if (cardName == '') {
-        alert('Enter card name first');
+        bodyDiv.style.padding = '141px 4px 4px 4px';
+        errorText1.innerHTML = 'Enter task name first';
+        errorText1.style.display = 'block';
     } else {
+        bodyDiv.style.padding = '124px 4px 4px 4px';
+        errorText1.style.display = 'none';
         cardBox.appendChild(cardColumn);
     }
 }
