c2d8ce2c0138aebb2d68de5a3d418d47fa74fbfa
mukesh.kumar <mukesh.kumar@decimal.co.in>
2020-04-28 11:20:52 +0530

    remove button added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,118 @@
+// function addRow() {
+//     var cardName = document.getElementById('card-name').value;
+//     var cardHeader = document.createElement('div');
+//     cardHeader.classList.add('card-header');
+//     cardHeader.classList.add('mb-1')
+//     cardHeader.innerHTML = cardName;
+
+//     var addCardBtn = document.createElement('input');
+//     addCardBtn.setAttribute('placeholder', 'Add a Card');
+//     addCardBtn.classList.add('task-card-input');
+//     addCardBtn.classList.add('mr-05');
+//     addCardBtn.setAttribute('id', 'task-name');
+
+//     var errorText = document.createElement('span');
+//     errorText.classList.add('error-text');
+
+//     var addBtn = document.createElement('button');
+//     addBtn.classList.add('btn-primary');
+//     addBtn.innerHTML = 'Add';
+//     addBtn.addEventListener('click', function () {
+//         var taskName = addCardBtn.value;
+//         var cardList = document.createElement('div');
+//         cardList.classList.add('card');
+//         cardList.classList.add('p-1');
+//         cardList.classList.add('mb-1');
+//         cardList.innerHTML = taskName;
+//         if (taskName == '') {
+//             errorText.innerHTML = 'Enter task name first';
+//             errorText.style.display = 'block';
+//         } else {
+//             errorText.style.display = 'none';
+//             cardBody.appendChild(cardList);
+//         }
+//     });
+
+//     var taskCardList = [];
+//     var taskCardDetails = {
+//         'name' : cardName,
+//         'type' : 'todo'
+//     }
+//     for(var i = 0; i < taskCardDetails; i++) {}
+//     // taskCardList.push(taskCardDetails);
+//     console.log(taskCardDetails);
+
+//     var cardBody = document.createElement('div');
+//     cardBody.classList.add('card-body');
+
+//     var card = document.createElement('div');
+//     card.classList.add('card');
+//     card.classList.add('p-1');
+//     card.setAttribute('id', 'card');
+//     card.appendChild(cardHeader);
+//     card.appendChild(cardBody);
+//     card.appendChild(addCardBtn);
+//     card.appendChild(addBtn);
+//     card.appendChild(errorText);
+
+//     var cardColumn = document.createElement('div');
+//     cardColumn.classList.add('task-card');
+//     cardColumn.classList.add('p-04');
+//     cardColumn.appendChild(card);
+
+//     var cardBox = document.getElementById('todo-box');
+//     var errorText1 = document.getElementById('error-text1');
+//     var bodyDiv = document.getElementById('body-div');
+
+//     if (cardName == '') {
+//         bodyDiv.style.padding = '141px 4px 4px 4px';
+//         errorText1.innerHTML = 'Enter task name first';
+//         errorText1.style.display = 'block';
+//     } else if (taskCardDetails[name] == cardName) {
+//         errorText1.innerHTML = 'Task already exist.';
+//     } else {
+//         bodyDiv.style.padding = '124px 4px 4px 4px';
+//         errorText1.style.display = 'none';
+//         cardBox.appendChild(cardColumn);
+//     }
+// }
+
+
+var taskCardList = [];
+
 function addRow() {
     var cardName = document.getElementById('card-name').value;
+    var errorText1 = document.getElementById('error-text1');
+
+    var taskCardDetails = {
+        'name': cardName,
+        'type': 'todo'
+    }
+    for (var i = 0; i < taskCardList.length; i++) {
+        // if (taskCardList[i].name == taskCardDetails.name) {
+        //     errorText1.innerHTML = 'red';
+        // } else {
+        //     taskCardList.push(taskCardDetails);
+        // }
+        if (taskCardList[i].name != taskCardDetails.name)
+        taskCardList.push(taskCardDetails);
+    }
+    taskCardList.push(taskCardDetails);
+    console.log(taskCardList);
+
+    var cardHeaderName = document.createElement('strong');
+    cardHeaderName.innerHTML = taskCardList[i].name;
+
+    var removeBtn = document.createElement('a');
+    removeBtn.setAttribute('href', 'javascript:void(0)');
+    removeBtn.classList.add('btn-remove');
+    removeBtn.innerHTML = svgJson.svg;
+    removeBtn.addEventListener('click', function() {});
+
     var cardHeader = document.createElement('div');
     cardHeader.classList.add('card-header');
-    cardHeader.classList.add('mb-1')
-    cardHeader.innerHTML = cardName;
+    cardHeader.appendChild(cardHeaderName);
+    cardHeader.appendChild(removeBtn);
 
     var addCardBtn = document.createElement('input');
     addCardBtn.setAttribute('placeholder', 'Add a Card');
@@ -22,7 +131,6 @@ function addRow() {
         var cardList = document.createElement('div');
         cardList.classList.add('card');
         cardList.classList.add('p-1');
-        cardList.classList.add('mb-1');
         cardList.innerHTML = taskName;
         if (taskName == '') {
             errorText.innerHTML = 'Enter task name first';
@@ -35,34 +143,39 @@ function addRow() {
 
     var cardBody = document.createElement('div');
     cardBody.classList.add('card-body');
+    cardBody.classList.add('p-1');
+
+    var cardFooter = document.createElement('div');
+    cardFooter.classList.add('card-footer');
+    cardFooter.classList.add('p-1');
+    cardFooter.appendChild(addCardBtn);
+    cardFooter.appendChild(addBtn);
+    cardFooter.appendChild(errorText);
 
     var card = document.createElement('div');
     card.classList.add('card');
-    card.classList.add('p-1');
     card.setAttribute('id', 'card');
     card.appendChild(cardHeader);
     card.appendChild(cardBody);
-    card.appendChild(addCardBtn);
-    card.appendChild(addBtn);
-    card.appendChild(errorText);
+    card.appendChild(cardFooter);
 
     var cardColumn = document.createElement('div');
     cardColumn.classList.add('task-card');
     cardColumn.classList.add('p-04');
     cardColumn.appendChild(card);
 
-    var cardBox = document.getElementById('card-box');
-    var errorText1 = document.getElementById('error-text1');
+    var cardBox = document.getElementById('todo-box');
     var bodyDiv = document.getElementById('body-div');
 
     if (cardName == '') {
         bodyDiv.style.padding = '141px 4px 4px 4px';
         errorText1.innerHTML = 'Enter task name first';
         errorText1.style.display = 'block';
+    } else if (taskCardList[i] == taskCardDetails.name) {
+        errorText1.innerHTML = 'Task already exist.';
     } else {
         bodyDiv.style.padding = '124px 4px 4px 4px';
         errorText1.style.display = 'none';
         cardBox.appendChild(cardColumn);
     }
 }
-
